d387cf7d2082cce59e396586c5c7064fb6a6580d
2351050186tram@ou.edu.vn <2351050186tram@.ou.edu.vn>
2026-04-03 15:12:10 +0700

    'update'

---
+++ eapp/utils.py
@@ -0,0 +1,10 @@
+def stats_cart(cart_items):
+    return {
+        'total_quantity': len(cart_items) if cart_items else 0
+    }
+
+def can_borrow(currently_borrowed, in_cart_count, limit=5):
+    return (currently_borrowed + in_cart_count) < limit
+
+def is_over_limit(currently_borrowed, in_cart_count, limit=5):
+    return (currently_borrowed + in_cart_count) > limit
